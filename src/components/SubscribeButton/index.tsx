66d7119a01d2a9eb3bd51f5aefc44754be537cea
Lubni Morais <lubni.morais@gmail.com>
2022-01-09 19:22:05 -0300

    Implementing integration with Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,42 @@
+import { useCallback } from 'react';
+import { useSession, signIn } from 'next-auth/react';
+
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
+
 
 interface ISubscripeButtonProps {
   priceId: string;
 }
 
 const SubscribeButton = ({ priceId }: ISubscripeButtonProps) => {
+  const { data: session} = useSession();
+
+  const handleSubscribe = useCallback(async () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }, [session])
+
   return (
     <button
       type='button'
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
